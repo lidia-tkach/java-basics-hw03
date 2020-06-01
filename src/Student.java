ede5e06089685654761d2880be18667e0b75ffd7
Lidia Tkach <tkach.lidia61@gmail.com>
2020-06-01 10:43:45 +0300

    test

--- src/Student.java
+++ src/Student.java
@@ -6,7 +6,7 @@ public class Student {
     private int rating;
 
     // TODO implement Student class according to the instructions provided in the README.md file
-
+    // test
 
     public Student(String name) {
         this.name = name;
